0aa1d997cf7cf72c53dacbddd5947437c49276a0
jwgatt <79326421+jwgatt@users.noreply.github.com>
2022-06-30 22:09:28 +0200

    Navbar styling

--- gistreet_vue/src/App.vue
+++ gistreet_vue/src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id='wrapper'>
-    <nav class='navbar is-dark'>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Gi-Street</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
     </nav>
   </div>
 </template>
 
 <style lang="scss">
-@import '..node_modules/bulma';
+@import '../node_modules/bulma'
 </style>
