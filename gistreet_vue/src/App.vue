2da09b0ccedd221bf311a477b778efbd9adbf23e
jwgatt <79326421+jwgatt@users.noreply.github.com>
2022-06-30 22:28:03 +0200

    Add shopping cart

--- gistreet_vue/src/App.vue
+++ gistreet_vue/src/App.vue
@@ -5,7 +5,42 @@
         <router-link to="/" class="navbar-item"><strong>Gi-Street</strong></router-link>
         <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
           <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span><template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Gi-Street</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
           <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/summer" class="navbar-item">Winter</router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light"> Log in</router-link>
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../node_modules/bulma'
+</style>
           <span aria-hidden="true"></span>
         </a>
       </div>
