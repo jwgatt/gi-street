84b26728258796741919e7e009217733ed25dc3c
jwgatt <79326421+jwgatt@users.noreply.github.com>
2022-07-01 09:42:49 +0200

    Show menu set to false
    Show menu set to false on default for mobile devices

--- gistreet_vue/src/App.vue
+++ gistreet_vue/src/App.vue
@@ -22,7 +22,57 @@
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/summer" class="navbar-item">Winter</router-link>
 
+          <div class="navbar-item"><template>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item"><strong>Gi-Street</strong></router-link>
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu=!showMobileMenu">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active':showMobileMenu}">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">Summer</router-link>
+          <router-link to="/summer" class="navbar-item">Winter</router-link>
+
           <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light"> Log in</router-link>
+
+              <router-link to="/cart" class="button is-success">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view />
+    </section>
+  </div>
+</template>
+
+<script >
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../node_modules/bulma'
+</style>
             <div class="buttons">
               <router-link to="/log-in" class="button is-light"> Log in</router-link>
               <router-link to="/cart" class="button is-success">
