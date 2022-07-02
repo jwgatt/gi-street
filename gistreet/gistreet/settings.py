315050d98a49c406ce290d0456b59e23a6e1727a
jwgatt <79326421+jwgatt@users.noreply.github.com>
2022-07-02 10:42:05 +0200

    set media routes

--- gistreet/gistreet/settings.py
+++ gistreet/gistreet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from re import M
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,15 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+
+    'product'
 ]
 
-CORS_ALLOWED_ORIGINS=[
-        'http://localhost:8080']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
