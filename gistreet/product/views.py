8080714d2b5a9aa8ccc5e7117241aabbdff374be
jwgatt <79326421+jwgatt@users.noreply.github.com>
2022-07-04 23:14:17 +0200

    create latest product set

--- gistreet/product/views.py
+++ gistreet/product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serialzers import ProductSerializer
+from .models import Product
+
+
+class LatestProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4]
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
